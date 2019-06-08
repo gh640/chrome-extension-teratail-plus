69b2fb730fe19d95e6fcd56b0948c691b47e28f7
Goto Hayato <habita.gh@gmail.com>
2019-06-08 15:49:09 +0900

    ミュート解除の機能を設定ページに追加。

--- options.js
+++ options.js
@@ -3,7 +3,11 @@
  */
 const INPUT_ID = 'usernameInput';
 const LIST_ID = 'usernames';
-const BUTTON_ID = 'add';
+const ADD_BUTTON_ID = 'add';
+const UNMUTE_BUTTON_ID = 'unmute';
+const USERNAME_CLASS = 'username-row';
+const USERNAME_CHECK_CLASS = 'row-check';
+const USERNAME_LABEL_CLASS = 'row-label';
 const KEY = 'mutedUsernames';
 
 main();
@@ -14,15 +18,16 @@ main();
 function main() {
 
   // ユーザ一覧表示をリフレッシュ
-  // add ボタンにイベントリスナーを追加
-  document.addEventListener('DOMContentLoaded', refreshUsernames);
-  document.getElementById(BUTTON_ID).addEventListener('click', addUsername);
+  // add ボタンと unmute ボタンにイベントリスナーを追加
+  document.addEventListener('DOMContentLoaded', refreshList);
+  document.getElementById(ADD_BUTTON_ID).addEventListener('click', add);
+  document.getElementById(UNMUTE_BUTTON_ID).addEventListener('click', unmute);
 }
 
 /**
- * イベントリスナー: ユーザを追加する
+ * イベントリスナー: ユーザをミュート対象に追加する
  */
-async function addUsername(event) {
+async function add(event) {
   let username = document.getElementById(INPUT_ID).value;
   let mutedUsernames = await load(KEY);
 
@@ -33,16 +38,41 @@ async function addUsername(event) {
   if (!mutedUsernames.includes(username)) {
     mutedUsernames.unshift(username);
     await save(KEY, mutedUsernames);
-    refreshUsernames();
+    refreshList();
   }
 
   event.preventDefault();
 }
 
 /**
+ * イベントリスナー: 選択されたユーザをアンミュートする
+ */
+async function unmute(event) {
+  let checkboxEls = document.getElementsByName('unmute');
+  let usernamesToUnmute = [];
+
+  for (let checkboxEl of checkboxEls) {
+    if (checkboxEl.checked) {
+      usernamesToUnmute.push(checkboxEl.value);
+    }
+  }
+
+  let mutedUsernames = await load(KEY);
+  let newMutedUsernames = [];
+  for (let username of mutedUsernames) {
+    if (!usernamesToUnmute.includes(username)) {
+      newMutedUsernames.push(username);
+    }
+  }
+
+  await save(KEY, newMutedUsernames);
+  refreshList();
+}
+
+/**
  * イベントリスナー: ユーザ一覧表示をリフレッシュする
  */
-async function refreshUsernames() {
+async function refreshList() {
   let usernamesEl = document.getElementById(LIST_ID);
   let mutedUsernames = await load(KEY);
 
@@ -50,13 +80,32 @@ async function refreshUsernames() {
     mutedUsernames = [];
   }
 
+  // かんたんに実装するためいったんすべて削除して追加し直す
   while (usernamesEl.firstChild) {
     usernamesEl.removeChild(usernamesEl.firstChild);
   }
 
   mutedUsernames.forEach((username) => {
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.name = 'unmute';
+    checkbox.value = username;
+    checkbox.id = username;
+
+    let checkboxSpan = document.createElement('span');
+    checkboxSpan.appendChild(checkbox);
+    checkboxSpan.classList.add(USERNAME_CHECK_CLASS);
+
+    let label = document.createElement('label');
+    label.setAttribute('for', username);
+    label.innerText = username;
+    label.classList.add(USERNAME_LABEL_CLASS);
+
     let container = document.createElement('div');
-    container.innerText = username;
+    container.appendChild(checkboxSpan);
+    container.appendChild(label);
+    container.classList.add(USERNAME_CLASS);
+
     usernamesEl.appendChild(container);
   });
 }
